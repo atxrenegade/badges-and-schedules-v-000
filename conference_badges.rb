380c6c3b0bc62e83fa92d7ffffa84b1c4d4a19bc
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 19:12:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,12 +12,11 @@ end
 def assign_rooms(attendees)
   number = 0
   attendees.collect do |name|
-    number += 1
-    "Hello, #{name}! You'll be assigned to room #{number}!"
+    "Hello, #{name}! You'll be assigned to room #{number.each_with_index + 1}!"
   end
 end
 
 def printer(attendees)
-  puts badge_maker(attendees)
+  puts badge_maker.each {|name| }
   puts assign_rooms(attendees)
 end
